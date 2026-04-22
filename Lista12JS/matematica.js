a03b6830d71c47743a855ccec616b176d83c939c
Marianne Oliveira <mariannerodrigues0806@gmail.com>
2026-04-21 21:40:42 -0300

    Lista 12 iniciada

---
+++ Lista12JS/matematica.js
@@ -0,0 +1,8 @@
+function somar(numero1, numero2){
+    if (numero1 == null || numero1 == undefined){
+        div_msg.innerHTML = `Número 1 não pode ser nulo ou indefinido`;
+    }
+    else if (numero2 == null || numero2 == undefined){
+        div_msg.innerHTML = `Número 2 não pode ser nulo ou indefinido`;
+    }
+}
