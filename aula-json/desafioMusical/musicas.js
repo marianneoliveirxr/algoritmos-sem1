b41ebe51d23e1a4fb655e56cc41434180ebe3ca8
marianneoliveirxr <marianne.rodrigues@sptech.school>
2026-05-08 13:47:54 -0300

    Add: desafio musical JSON

---
+++ aula-json/desafioMusical/musicas.js
@@ -0,0 +1,82 @@
+const musicas = [
+    {
+        "titulo": "Yellow",
+        "banda": "Coldplay",
+        "duracao": "4min",
+        "anoLancamento": 2000,
+        "avaliacoes": [5, 5, 4, 5],
+        "integrantes": ["Chris Martin", "Jonny Buckland", "Guy Berryman", "Will Champion"]
+    },
+    {
+        "titulo": "Viva La Vida",
+        "banda": "Coldplay",
+        "duracao": "4min",
+        "anoLancamento": 2008,
+        "avaliacoes": [5, 5, 5, 4],
+        "integrantes": ["Chris Martin", "Jonny Buckland", "Guy Berryman", "Will Champion"]
+    },
+    {
+        "titulo": "Paradise",
+        "banda": "Coldplay",
+        "duracao": "4min",
+        "anoLancamento": 2011,
+        "avaliacoes": [5, 4, 5, 4],
+        "integrantes": ["Chris Martin", "Jonny Buckland", "Guy Berryman", "Will Champion"]
+    },
+    {
+        "titulo": "Lancinho",
+        "banda": "Turma do Pagode",
+        "duracao": "3min",
+        "anoLancamento": 2012,
+        "avaliacoes": [5, 4, 4, 5],
+        "integrantes": ["Leiz", "Rubinho", "Caramelo", "Thiagão"]
+    },
+    {
+        "titulo": "Camisa 10",
+        "banda": "Turma do Pagode",
+        "duracao": "3min",
+        "anoLancamento": 2013,
+        "avaliacoes": [5, 5, 4, 4],
+        "integrantes": ["Leiz", "Rubinho", "Caramelo", "Thiagão"]
+    },
+    {
+        "titulo": "Deixa em Off",
+        "banda": "Turma do Pagode",
+        "duracao": "3min",
+        "anoLancamento": 2020,
+        "avaliacoes": [4, 4, 5, 5],
+        "integrantes": ["Leiz", "Rubinho", "Caramelo", "Thiagão"]
+    },
+    {
+        "titulo": "Tá Vendo Aquela Lua",
+        "banda": "Exaltasamba",
+        "duracao": "4min",
+        "anoLancamento": 2009,
+        "avaliacoes": [5, 5, 5, 5],
+        "integrantes": ["Pericles", "Thiaguinho", "Pinha Presidente", "Thell"]
+    },
+    {
+        "titulo": "Livre Pra Voar",
+        "banda": "Exaltasamba",
+        "duracao": "4min",
+        "anoLancamento": 2003,
+        "avaliacoes": [5, 4, 5, 4],
+        "integrantes": ["Pericles", "Thiaguinho", "Pinha Presidente", "Thell"]
+    },
+    {
+        "titulo": "Californication",
+        "banda": "Red Hot Chili Peppers",
+        "duracao": "5min",
+        "anoLancamento": 1999,
+        "avaliacoes": [5, 5, 5, 5],
+        "integrantes": ["Anthony Kiedis", "Flea", "John Frusciante", "Chad Smith"]
+    },
+    {
+        "titulo": "Otherside",
+        "banda": "Red Hot Chili Peppers",
+        "duracao": "4min",
+        "anoLancamento": 1999,
+        "avaliacoes": [5, 4, 5, 5],
+        "integrantes": ["Anthony Kiedis", "Flea", "John Frusciante", "Chad Smith"]
+    }
+];
