ff7d3079cbf9f46b1ff428e87b48163e7e142339
marianneoliveirxr <marianne.rodrigues@sptech.school>
2026-05-13 22:42:17 -0300

    Lista13JS parte 1 finalizada

---
+++ aula-json/Lista13JS/lista13.js
@@ -0,0 +1,179 @@
+const alunos = [
+  {
+    nome: "Marianne",
+    ra: "04261096",
+    curso: "ciência da computação",
+    semestre: 1,
+  },
+  {
+    nome: "Amanda",
+    ra: "04261049",
+    curso: "Análise e Desenvolvimento de Sistemas",
+    semestre: 2,
+  },
+  {
+    nome: "Isabella",
+    ra: "04261052",
+    curso: "Sistemas de Informação e Inteligência Artificial",
+    semestre: 1,
+  },
+];
+
+const produtos = [
+  {
+    nome: "Caneta",
+    preco: 1.99,
+    quantidade: 50,
+  },
+  {
+    nome: "Lápis",
+    preco: 0.99,
+    quantidade: 30,
+  },
+  {
+    nome: "Caderno",
+    preco: 24,
+    quantidade: 40,
+  },
+];
+
+const cliente = [
+  {
+    nome: "Rafaela",
+    idade: 19,
+    endereco: {
+      rua: "Rua das Flores",
+      numero: 556,
+      cidade: "Campinas",
+    },
+  },
+  {
+    nome: "Paola",
+    idade: 23,
+    endereco: {
+      rua: "Rua das Margaridas",
+      numero: 880,
+      cidade: "São José do Rio Preto",
+    },
+  },
+  {
+    nome: "Lucas",
+    idade: 25,
+    endereco: {
+      rua: "Avenida Brasil",
+      numero: 900,
+      cidade: "Rio de Janeiro",
+    },
+  },
+];
+
+const conta = [
+  {
+    titular: "Marianne",
+    saldo: 1000,
+    banco: "Itau",
+  },
+];
+
+const contatos = [
+  {
+    nome: "Pietra",
+    telefone: "(11)96618-1418",
+  },
+  {
+    nome: "Gabriela",
+    telefone: "(11)94596-9535",
+  },
+  {
+    nome: "Giovanna",
+    telefone: "(11)99668-2324",
+  },
+  {
+    nome: "Maria",
+    telefone: "(11)98334-7700",
+  },
+];
+
+const turma = [
+  {
+    nome: "Gabriel",
+    nota: 4,
+  },
+  {
+    nome: "Lucas",
+    nota: 7,
+  },
+  {
+    nome: "Luiza",
+    nota: 9,
+  },
+  {
+    nome: "Marina",
+    nota: 10,
+  },
+  {
+    nome: "Leticia",
+    nota: 8,
+  },
+];
+
+const produtos2 = [
+  {
+    nome: "Geladeira",
+    categoria: "Eletronicos",
+    preco: 3.9,
+  },
+  {
+    nome: "Nike Dunk Branco",
+    categoria: "Calçados",
+    preco: 500,
+  },
+  {
+    nome: "Cobertor",
+    categoria: "Casa",
+    preco: 200,
+  },
+  {
+    nome: "Caderno",
+    categoria: "Papelaria",
+    preco: 24.99,
+  },
+  {
+    nome: "Colchão",
+    categoria: "Casa",
+    preco: 1000,
+  },
+  {
+    nome: "Televisão",
+    categoria: "Eletronicos",
+    preco: 2000,
+  },
+];
+
+const cardapio = [
+  {
+    nome: "Bruschetta Italiana",
+    tipo: "Entrada",
+    preco: 18.9,
+  },
+  {
+    nome: "Lasanha à Bolonhesa",
+    tipo: "Prato Principal",
+    preco: 42.5,
+  },
+  {
+    nome: "Salmão Grelhado com Legumes",
+    tipo: "Prato Principal",
+    preco: 56.0,
+  },
+  {
+    nome: "Cheesecake de Frutas Vermelhas",
+    tipo: "Sobremesa",
+    preco: 22.0,
+  },
+  {
+    nome: "Petit Gateau com Sorvete",
+    tipo: "Sobremesa",
+    preco: 24.9,
+  },
+];
